3ac77afd27f3a1dd0ac901543286bd6ec67d5ab5
Anand Sai Mishra <anandsai.mishra@gmail.com>
2020-11-19 16:56:41 -0800

    Test 3

---
+++ iHub/TesterTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TesterTableViewCell.swift
+//  iHub
+//
+//  Created by Anand Sai Mishra on 11/19/20.
+//
+
+import UIKit
+
+class TesterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
