2a07dca843a91ad7b8c25ec334f33e184ed8e2da
Anand Sai Mishra <anandsai.mishra@gmail.com>
2020-11-19 16:50:21 -0800

    Testing commit

---
+++ iHub/TwoittedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TwoittedTableViewCell.swift
+//  iHub
+//
+//  Created by Anand Sai Mishra on 11/19/20.
+//
+
+import UIKit
+
+class TwoittedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
